1226456d767e0f2b3bba9cb7e99e0307f0c00c08
sttagami2 <sttagami2@gmail.com>
2021-03-28 18:52:36 +0900

    finish lesson10

--- hello_rectangle/lib/main.dart
+++ hello_rectangle/lib/main.dart
@@ -6,7 +6,6 @@ void main() {
   ));
 }
 
-
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -16,25 +15,20 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body: Center(
-        child: IconButton(
-          onPressed: () {
-            print('you clicked me');
-          },
-          icon: Icon(Icons.alternate_email),
-          color: Colors.amber,
-        ),
-        // child: RaisedButton.icon(
-        //   onPressed: () {},
-        //   icon: Icon(
-        //     Icons.mail
-        //   ),
-        //   label: Text('maill me'),
-        //   color: Colors.amber,
-        // ),
+      body: Padding(
+        padding: EdgeInsets.all(90.0),
+        child: Text('hello'),
       ),
+      // body: Container(
+      //   // padding: EdgeInsets.all(20.0),
+      //   // padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+      //   padding: EdgeInsets.fromLTRB(10.0, 20.0, 30.0, 40.0),
+      //   margin: EdgeInsets.all(30.0),
+      //   color: Colors.grey[400],
+      //   child: Text('hello'),
+      // ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => {},
+        onPressed: () {},
         child: Text('click'),
         backgroundColor: Colors.red[600],
       ),
